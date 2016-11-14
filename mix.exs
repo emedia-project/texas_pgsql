05d2336207440854127c1e64c422d1bc213d578b
glejeune <gregoire.lejeune@free.fr>
2016-11-14 18:24:10 +0100

    Bump bucs

--- mix.exs
+++ mix.exs
@@ -8,7 +8,8 @@ defmodule Texas.Pgsql.Mixfile do
       elixir: "~> 1.2",
       build_embedded: Mix.env == :prod,
       start_permanent: Mix.env == :prod,
-      deps: deps
+      deps: deps,
+      aliases: aliases
     ]
   end
 
@@ -21,10 +22,36 @@ defmodule Texas.Pgsql.Mixfile do
 
   defp deps do
     [
-      {:lager, "~> 3.2"},
+      {:lager, "~> 3.2.0"},
       {:texas_adapter, git: "https://github.com/emedia-project/texas_adapter.git", branch: "master"},
-      {:bucs, "~> 0.1.0"},
+      {:bucs, "~> 1.0.0"},
       {:epgsql, "~> 3.2"}    
     ]
   end
+
+  defp aliases do
+    [compile: [&pre_compile_hooks/1, "compile", &post_compile_hooks/1]]
+  end
+
+  defp pre_compile_hooks(_) do
+    run_hook_cmd [
+    ]
+  end
+
+  defp post_compile_hooks(_) do
+    run_hook_cmd [
+    ]
+  end
+
+  defp run_hook_cmd(commands) do
+    {_, os} = :os.type
+    for command <- commands, do: (fn
+      ({regex, cmd}) ->
+         if Regex.match?(Regex.compile!(regex), Atom.to_string(os)) do
+           Mix.Shell.cmd cmd, [], fn(x) -> Mix.Shell.IO.info(String.strip(x)) end
+         end
+      (cmd) ->
+        Mix.Shell.cmd cmd, [], fn(x) -> Mix.Shell.IO.info(String.strip(x)) end
+      end).(command)
+  end    
 end
